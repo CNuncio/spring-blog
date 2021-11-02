250ae93791fdb1ed454bc920e92f7433bc9d6066
Christina Nuncio <nuncio.christina01@gmail.com>
2021-11-02 15:52:29 -0500

    unfinished coffee springboot lecture

--- src/main/java/com/codeup/springblog/controllers/CoffeeController.java
+++ src/main/java/com/codeup/springblog/controllers/CoffeeController.java
@@ -3,8 +3,6 @@ package com.codeup.springblog.controllers;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import org.springframework.stereotype.Controller;
 
@@ -17,13 +15,38 @@ public class CoffeeController {
 
     @GetMapping("/coffee/{roast}")
     public String roastSelection(@PathVariable String roast, Model model) {
-        model.addAttribute("roast", roast);
+//        model.addAttribute("roast", roast);
+
+
 //        boolean choseICED = false;
 //        if (roast.equals("ICED")) {
 //            choseICED = true;
 //        }
 //        model.addAttribute("choseICED", choseICED);
         // to make html changes
+
+        ////
+
+
+        Coffee selection = new Coffee(roast, "Cool Beans");
+        Coffee selection2 = new Coffee(roast, "Jolting Joe");
+//        selection.setRoast(roast);
+        if (roast.equals("dark")) {
+            selection.setOrigin("Colombia");
+            selection.setOrigin("Brazil");
+        } else if (roast.equals("medium")) {
+            selection.setOrigin("New Guinea");
+            selection.setOrigin("Sumatra");
+        } else {
+            selection.setOrigin("Kenya");
+            selection.setOrigin("Ethiopia");
+        }
+        List<Coffee> selections = new ArrayList<>();
+        selections.add(selection);
+        selections.add(selection2);
+        model.addAttribute("selection", selection);
+        model.addAttribute("roast", roast);
+
         return "views-lecture/coffee";
     }
 }
